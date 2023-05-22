5f0c56f60b9235dc96255b10b13311d4ed4b977f
bigxshowx <bigxshowx@gmail.com>
2023-05-22 18:40:52 +0900

    pushing final code for warm up "War game"

--- War_Card_Game.py
+++ War_Card_Game.py
@@ -1,66 +1,7 @@
-print("okay \n" * 100)
+from War_Card_Game import *
 
-# Card Class, to know suit, rank, integer value
-
-import random
-
-values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7,
-          "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 11, "Queen": 12, "King": 13, "Ace": 14}
-
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ("Two", "Three", "Four", "Five", "Six", "Seven",
-         "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace")
-
-
-class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
-        self.value = values[rank]
-
-    def __str__(self):
-        return self.rank + " of " + self.suit
-
-
-class Deck():
-
-    def __init__(self):
-
-        self.all_cards = []
-
-        for suit in suits:
-            for rank in ranks:
-                # Create Card Object
-                created_card = Card(suit, rank)
-                self.all_cards.append(created_card)
-
-    def shuffle_deck(self):
-
-        random.shuffle(self.all_cards)
-
-    def deal_one(self):
-        return self.all_cards.pop()
-
-
-class Player():
-
-    def __init__(self, name):
-        self.name = name
-        self.all_cards = []
-
-    def remove_one(self):
-        return self.all_cards.pop(0)
-
-    def add_cards(self, new_cards):
-        if type(new_cards) == list:
-            self.all_cards.extend(new_cards)
-        else:
-            self.all_cards.append(new_cards)
-
-    def __str__(self):
-        return f'Player {self.name} has {len(self.all_cards)} card(s).'
-
-# Game setup
+# Game setup and Play
+print("Game Started")
 player_one = Player("One")
 player_two = Player("Two")
 
@@ -78,15 +19,17 @@ round_num = 0
 # Game Play Logic: while loop for game on
 while game_on:
     round_num += 1
-    print(f'Round {round_num}.')
+    # print(f'Round {round_num}.')
 
     if len(player_one.all_cards) == 0:
-        print('Player One, out of cards, player Two wins!')
+        print(f'Player One is out of cards, player **TWO** wins in round {round_num}!')
         game_on = False
+        break
 
     if len(player_two.all_cards) == 0:
-        print('Player Two, out of cards, player One wins!')
+        print(f'Player Two is out of cards, player **ONE** wins in round {round_num}!')
         game_on = False
+        break
 
     # Start a new round
     player_one_cards = []
@@ -100,4 +43,45 @@ while game_on:
 
     while at_war:
 
-        if player_one.all_cards[-1] >
+        # print("war one", len(player_one.all_cards))
+        # print("war one", len(player_two.all_cards))
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            # print("player 1 wins round")
+            at_war = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            # print("player 2 wins round")
+            at_war = False
+
+        else:
+            print("WAR ! ! ! ")
+
+            if len(player_one.all_cards) < 5:
+                print("Player One has run out of material... \n")
+                print(f"Player Two wins in round {round_num}.")
+                game_on = False
+                break
+
+            elif len(player_two.all_cards) < 5:
+                print("Player Two has run out of material... \n")
+                print(f"Player One wins in round {round_num}.")
+                game_on = False
+                break
+
+            else:
+                for num in range(5):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
+
+print('Game Over')
+
+# Key Take-aways:
+#  1) The Deck Class can use instances of the Card Class.
+#  2) The Player class could hold instances of the Card class as well within it's own card list.
